7d4783707d9412621646befd4c0df0e134046bcf
EbtihalMoosa <138448703+EbtihalMoosa@users.noreply.github.com>
2023-08-28 03:41:13 -0700

    Add files via upload
    Adam, Bella, Carl collect token in game, 
    Bella has 10 more tokens than Adam
    Carl has 8 more tokens than Bella
    altogether they have 91 tokens
    how many tokens does Adam have

---
+++ main.dart
@@ -0,0 +1,71 @@
+//Adam, Bella, Carl collect token in game, 
+//Bella has 10 more tokens than Adam
+//Carl has 8 more tokens than Bella
+// altogether they have 91 tokens
+//how many tokens does Adam have
+
+void main() {
+//   int a; 
+//   int b; 
+//   int c;
+
+//   for (a = 0; a <= 91; a++) {
+//     b = a + 10;
+//     c = b + 8;
+
+//     if (a + b + c == 91) {
+//       break;
+//     }
+//   }
+
+//   print("Adam = $a");
+// }
+
+//OR------------------------------------------------
+
+
+  double? carl;
+  double? adam;
+  double? bella;
+  double? tokens = 91;
+
+
+//carl
+//carl addetion = 8+10 =18
+//bella
+// bella addetion = 10
+//the sum of addition = 10 + 18
+  double? add= 28;
+ double? remaind;
+ remaind = tokens - add;
+
+
+
+
+
+ double? remaind1;
+ remaind1 = remaind/3;
+
+
+
+
+ //carl
+
+
+ carl= remaind1 + 18;
+ print(carl);
+
+//adam
+
+adam =remaind1;
+
+print(adam);
+
+// bella
+bella = remaind1 + 10;
+print(bella);
+
+
+
+
+}
